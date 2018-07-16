0be16846ac5f1decc75ab456e31d23dbbdb19b6b
Nattapon Benz <sevipsagis@gmail.com>
2018-07-16 14:11:59 +0700

    add function to create task card and add to list

--- script.js
+++ script.js
@@ -20,9 +20,41 @@ function updateTask(){
     var doneTask = doneBox.childElementCount;
     console.log(todoTask, progressTask, doneTask);
     todoNum.innerText = `${todoTask} task`;
-    progressNum.innerText = `${progressTask} task`;
+    progressNum.innerText = `  ${progressTask} task`;
     doneNum.innerText = `${doneTask} task`;
 }
-$(document).ready(function(){
+$(document).ready(() => {
     updateTask();
+    var ID_NUMBER = 1;
+    function addTask(taskDesc){
+        var taskList = document.createElement("li");
+        var taskText = document.createTextNode(taskDesc);
+        taskList.appendChild(taskText);
+        taskList.setAttribute("id", ID_NUMBER);
+        taskList.classList.add("list-group-item");
+        taskList.classList.add("list-group-item-action");
+        taskList.classList.add("list-group-item-info");
+        taskList.setAttribute("draggable", true);
+        taskList.setAttribute("ondragstart", "drag(event)");
+        ID_NUMBER++;
+        return taskList;
+    }
+    $("#todoButton").on("click", function(){
+        var desc = prompt("Type a name for your task....");
+        var item = addTask(desc);
+        todoBox.appendChild(item);
+        updateTask();
+    });
+    $("#progressButton").on("click", function(){
+        var desc = prompt("Type a name for your task....");
+        var item = addTask(desc);
+        progressBox.appendChild(item);
+        updateTask();
+    });
+    $("#doneButton").on("click", function(){
+        var desc = prompt("Type a name for your task....");
+        var item = addTask(desc);
+        doneBox.appendChild(item);
+        updateTask();
+    });
 });
